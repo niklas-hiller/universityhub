0764cf7ef16cd0555c46b0e15d72600e7c9405e6
Niklas Hiller <niklas.hiller1998@gmail.com>
2023-03-24 10:04:11 +0100

    feat: minor adjustments

--- University.Server/Domain/Models/EModuleStatus.cs
+++ University.Server/Domain/Models/EModuleStatus.cs
@@ -5,35 +5,27 @@ namespace University.Server.Domain.Models
     public enum EModuleStatus : byte
     {
         // EDUCATES | Unterrichtet Modul
-        [Description("Educates")]
         Educates = 0,
 
         // ENROLLED | Angemeldet zur Prüfung
-        [Description("Enrolled")]
         Enrolled = 1,
 
         // ENROLMENT | Angemeldet
-        [Description("Enrolment")]
         Enrolment = 2,
 
         // PASSED | Bestanden
-        [Description("Passed")]
         Passed = 3,
 
         // FAILED | Nicht Bestanden
-        [Description("Failed")]
         Failed = 4,
 
         // EXCLUDED | Prüfungszulassung entzogen
-        [Description("Excluded")]
         Excluded = 5,
 
         // TRANSFERRED | Angerechnet / Anerkannt
-        [Description("Transferred")]
         Transferred = 6,
 
         // MISSING | Ergebnis noch ausstehend
-        [Description("Missing")]
         Missing = 7,
     }
 }
