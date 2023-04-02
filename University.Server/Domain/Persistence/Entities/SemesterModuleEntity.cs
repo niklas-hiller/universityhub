50f39b10700a1efe76e8bfe69c4bd9ba110e6849
Niklas Hiller <niklas.hiller1998@gmail.com>
2023-04-02 16:45:21 +0200

    fix: null guid

--- University.Server/Domain/Persistence/Entities/SemesterModuleEntity.cs
+++ University.Server/Domain/Persistence/Entities/SemesterModuleEntity.cs
@@ -3,7 +3,7 @@
     public class SemesterModuleEntity : BaseEntity
     {
         public ICollection<LectureEntity> Lectures { get; set; } = new List<LectureEntity>();
-        public Guid? ProfessorId { get; set; }
+        public Guid ProfessorId { get; set; } = Guid.Empty;
         public Guid ModuleId { get; set; }
     }
 }
